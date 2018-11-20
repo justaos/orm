e3ef7e7a033fbf7b44772ade78b8593972e12b0f
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-11-20 17:14:38 +0530

    Update database.service.ts

--- src/database.service.ts
+++ src/database.service.ts
@@ -5,7 +5,7 @@ import DatabaseConfiguration from "./database-configuration";
 export class DatabaseService {
 
     static connect(config: any): Promise<any> {
-        let dbConfig = new DatabaseConfiguration(config);
+        let dbConfig = new DatabaseConfiguration(config.host, config.port, config.name, config.user, config.password, config.dialect);
         let dbConnector = new DatabaseConnector(dbConfig);
         return dbConnector.connect(); // returns promise
     }
