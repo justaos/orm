c46a180290d3c7e2fe2e297c9f9bd4c258e24385
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2019-03-06 15:23:55 +0530

    Update anysols-model.js

--- src/anysols-model.js
+++ src/anysols-model.js
@@ -1,14 +1,18 @@
 import DatabaseConfiguration from "./database-configuration";
+import DatabaseConnector from "./database-connector";
 
 export default class AnysolsModel {
 
     private dbConfig: DatabaseConfiguration;
+    private conn: DatabaseConnection;
     
     constructor(config) {
         this.dbConfig = new DatabaseConfiguration(config.host, config.port, config.name, config.user, config.password, config.dialect);
     }
 
     async connect() {
+        this.conn = await new DatabaseConnection(this.dbConfig).connect();
+        return this.conn;
     }
 
     async databaseExists() {
