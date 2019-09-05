377b1650e86a11239a0eb7a175eb8cbedff9abec
Shivaji Varma <contact@shivajivarma.com>
2019-09-05 08:45:57 +0530

    version bump

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import AnysolsODM from "./anysolsODM";
+import AnysolsCursor from "./cursor/anysolsCursor";
+import AnysolsCollection from "./collection/anysolsCollection";
+import AnysolsRecord from "./record/anysolsRecord";
 import FieldType from "./field-types/fieldType.interface";
 import OperationInterceptor from "./operation-interceptor/operationInterceptor";
-import {DataType, StringDataType} from "./core";
-import AnysolsCollection from "./collection/anysolsCollection";
+import {DataType, DateDataType, IntegerDataType, ObjectDataType, StringDataType} from "./core";
 import {OPERATION_WHEN, OPERATIONS} from "./constants";
 
-
-export {AnysolsODM, AnysolsCollection, FieldType, OperationInterceptor, StringDataType, DataType, OPERATION_WHEN, OPERATIONS}
+export {
+    AnysolsODM,
+    AnysolsCollection,
+    AnysolsRecord,
+    AnysolsCursor,
+    FieldType,
+    OperationInterceptor,
+    StringDataType,
+    DataType,
+    ObjectDataType,
+    DateDataType,
+    IntegerDataType,
+    OPERATION_WHEN,
+    OPERATIONS,
+}
