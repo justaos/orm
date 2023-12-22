87992a0c4b1fa9dadfb1315609c456481c120f7d
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2023-12-22 16:52:23 +0530

    remove log formatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
-import { format } from "npm:sql-formatter";
 import { Logger } from "../deps.ts";
 
 export function logSQLQuery(logger: Logger, query: string) {
-  return logger.debug("\n" + format(query));
+  return logger.debug("\n" + query);
 }
