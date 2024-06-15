f928a490db0b83cfa5d9c3333cd007e2b11a12f5
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-06-15 14:57:10 +0530

    feat: string type undefined issue fix

--- src/data-types/types/StringDataType.ts
+++ src/data-types/types/StringDataType.ts
@@ -13,7 +13,7 @@ export default class StringDataType extends DataType {
   validateDefinition(_definition: ColumnDefinitionInternal) {}
 
   setValueIntercept(value: string | number | null): string | null {
-    if (value === null) return null;
+    if (value === null || typeof value === "undefined") return null;
     return String(value);
   }
 
